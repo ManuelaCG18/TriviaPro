a599cd0d4cff53bd82d6becc785146a4dad0ef47
Manuela Castaño G <manuela.castano585@pascualbravo.edu.co>
2025-04-07 14:25:28 -0500

    Agregar archivos de proyecto.

---
+++ TriviaPro/Pregunta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaPro
+{
+    public abstract class Pregunta
+    {
+        public string Texto {  get; protected set; }
+        public List<string> Opciones { get; protected set; }
+        public string RespuestaCorrecta { get; protected set; }
+        public string Categoria { get; protected set; }
+
+        public string Nivel { get; protected set; }
+
+        public int TiempoLimite { get; protected set; }
+
+        public abstract int PuntajeCorrecto { get; }
+        public int PuntajeIncorrecto => -2;
+
+    }
+}

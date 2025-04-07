ba14206b89be28805501dcdf8af660e3e910a4fc
Manuela Castaño G <manuela.castano585@pascualbravo.edu.co>
2025-04-07 14:50:31 -0500

    Creacion de clase base Pregunta

--- TriviaPro/Pregunta.cs
+++ TriviaPro/Pregunta.cs
@@ -8,17 +8,38 @@ namespace TriviaPro
 {
     public abstract class Pregunta
     {
+        // propiedades de pregunta
         public string Texto {  get; protected set; }
         public List<string> Opciones { get; protected set; }
-        public string RespuestaCorrecta { get; protected set; }
+        public int RespuestaCorrecta { get; protected set; }
         public string Categoria { get; protected set; }
-
         public string Nivel { get; protected set; }
-
         public int TiempoLimite { get; protected set; }
 
+
+        // propiedades abstractas de las clases hijas
         public abstract int PuntajeCorrecto { get; }
         public int PuntajeIncorrecto => -2;
 
+
+        // constructor
+        public Pregunta(string texto, List<string> opciones, int respuestaCorrecta, string categoria, string nivel)
+        {
+            Texto = texto ?? throw new ArgumentNullException(nameof(texto));
+            Opciones = opciones ?? throw new ArgumentNullException(nameof(opciones));
+            RespuestaCorrecta = respuestaCorrecta;
+            Categoria = categoria ?? throw new ArgumentNullException(nameof(categoria));
+            Nivel = nivel ?? throw new ArgumentNullException(nameof(nivel));
+
+            if (opciones.Count < 2 || opciones.Count > 4)
+
+                throw new ArgumentException("Debe haber entre 2 y 4 opciones");
+        }
+
+        public virtual bool EvaluarRespuesta(int respuestaSeleccionada)
+        {
+            return respuestaSeleccionada == RespuestaCorrecta;
+        }
+       
     }
 }

a25b6c107376857fd0e9fb4ab81de1161a97a011
Manuela Castaño G <manuela.castano585@pascualbravo.edu.co>
2025-04-07 18:11:19 -0500

    Clase Jugador lista

---
+++ TriviaPro/Jugador.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaPro
+{
+    public class Jugador
+    {
+        public string Nombre { get; set; }
+        public int Puntaje { get; private set; }
+        public int PreguntasCorrectas { get; private set; }
+        public int PreguntasIncorrectas { get; private set; }
+
+        public void ActualizarPuntaje(int puntos)
+        {
+            Puntaje += puntos;
+            if (Puntaje < 0) Puntaje = 0; // El puntaje mínimo es 0
+
+            if (puntos > 0) PreguntasCorrectas++;
+            else PreguntasIncorrectas++;
+        }
+
+        public void Reiniciar()
+        {
+            Puntaje = 0;
+            PreguntasCorrectas = 0;
+            PreguntasIncorrectas = 0;
+        }
+    }
+}
